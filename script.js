a081263d1f6de7ac765a83fb145db71d326705ec
Finn <finnterdal@gmail.com>
2019-08-02 15:18:16 -0500

    script works in node but not in browser

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let cohort = '1907-FSA-CH-WEB-FT'
 
 console.log(`Welcome to Fullstack, ${cohort}!`)
 
-const welcome = document.getElementById('welcome')
+// const welcome = document.getElementById('welcome')
 
-welcome.innerText = cohort
+// welcome.innerText = cohort
+
+module.exports = {
+  cohort
+}
